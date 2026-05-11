791bff4e9b994c3220fc52e0d529ba8758955563
mkiar <mkiar6315@sdsu.edu>
2026-05-10 21:55:48 -0700

    Add WeeklyLog class to calculate weekly nutrition

---
+++ src/main/java/com/example/demo/WeeklyLog.java
@@ -0,0 +1,157 @@
+package com.example.demo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.DayOfWeek;
+import java.time.temporal.TemporalAdjusters;
+
+//Retrieves information of all food in current log database (All servings of food logged for the day)
+public class WeeklyLog {
+    String weekStart = java.time.LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)).toString();
+    String weekEnd = java.time.LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY)).toString();
+
+    public double getCalories() {
+
+        String sql = """
+                SELECT SUM(f.calories * l.servings)
+                FROM food_logs l
+                JOIN foods f ON l.food_id = f.id
+                WHERE l.date BETWEEN ? and ?
+                """;
+
+        return getValue(sql);
+    }
+
+    public double getProteins() {
+
+        String sql = """
+                SELECT SUM(f.protein * l.servings)
+                FROM food_logs l
+                JOIN foods f ON l.food_id = f.id
+                WHERE l.date BETWEEN ? and ?
+                """;
+
+        return getValue(sql);
+    }
+
+    public double getSugars() {
+
+        String sql = """
+                SELECT SUM(f.sugar * l.servings)
+                FROM food_logs l
+                JOIN foods f ON l.food_id = f.id
+                WHERE l.date BETWEEN ? and ?
+                """;
+
+        return getValue(sql);
+    }
+
+    public double getCarbs() {
+
+        String sql = """
+                SELECT SUM(f.carbs * l.servings)
+                FROM food_logs l
+                JOIN foods f ON l.food_id = f.id
+                WHERE l.date BETWEEN ? and ?
+                """;
+
+        return getValue(sql);
+    }
+
+    public double getFiber() {
+
+        String sql = """
+                SELECT SUM(f.fiber * l.servings)
+                FROM food_logs l
+                JOIN foods f ON l.food_id = f.id
+                WHERE l.date BETWEEN ? and ?
+                """;
+
+        return getValue(sql);
+    }
+
+    public double getSaturated_fat() {
+
+        String sql = """
+                SELECT SUM(f.saturated_fat * l.servings)
+                FROM food_logs l
+                JOIN foods f ON l.food_id = f.id
+                WHERE l.date BETWEEN ? and ?
+                """;
+
+        return getValue(sql);
+    }
+
+    public double getTrans_fat() {
+
+        String sql = """
+                SELECT SUM(f.trans_fat * l.servings)
+                FROM food_logs l
+                JOIN foods f ON l.food_id = f.id
+                WHERE l.date BETWEEN ? and ?
+                """;
+
+        return getValue(sql);
+    }
+
+
+    public double getFats() {
+
+        String sql = """
+                SELECT SUM(f.fats * l.servings)
+                FROM food_logs l
+                JOIN foods f ON l.food_id = f.id
+                WHERE l.date BETWEEN ? and ?
+                """;
+
+        return getValue(sql);
+    }
+
+    public double getSodium() {
+
+        String sql = """
+                SELECT SUM(f.sodium * l.servings)
+                FROM food_logs l
+                JOIN foods f ON l.food_id = f.id
+                WHERE l.date BETWEEN ? and ?
+                """;
+
+        return getValue(sql);
+    }
+
+    public double getCholesterol() {
+
+        String sql = """
+                SELECT SUM(f.cholesterol * l.servings)
+                FROM food_logs l
+                JOIN foods f ON l.food_id = f.id
+                WHERE l.date BETWEEN ? and ?
+                """;
+
+        return getValue(sql);
+    }
+
+    private double getValue(String sql) {
+
+        try (Connection conn = DatabaseManager.connect()) {
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             stmt.setString(1, weekStart);
+             stmt.setString(2, weekEnd);
+             ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                double value = rs.getDouble(1);
+                if (rs.wasNull()) {
+                    return 0;
+                }
+                return value;
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return 0;
+    }
+}
